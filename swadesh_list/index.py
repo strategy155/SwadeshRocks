c18b3ca756da422961e117320a67bf540198e786
gaurwaithmelui <strategy155@gmail.com>
2016-11-14 17:31:10 +0300

    finished login/register parts

--- swadesh_list/index.py
+++ swadesh_list/index.py
@@ -1,16 +1,20 @@
 from flask import request, render_template, session, redirect
 from swadesh_list import app
 from models import LoginForm, User
+from sqlalchemy.exc import InterfaceError
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = LoginForm(request.form)
-    print(User.query.all())
+
     if request.method == 'POST' and form.validate():
-        current_user = User.query.filter(User.name == form.username).one_or_none()
-        if current_user is None:
+        print(form.username.data)
+        try:
+            current_user = User.query.filter(User.name == form.username.data).first()
+            if current_user.password == form.password.data:
+                session['name'] = current_user.name
+                return render_template('index.html', name=current_user.name)
+        except InterfaceError:
             redirect('login')
-        if current_user.password == form.password:
-            session['name'] = current_user.username
-        return render_template('index.html', name=current_user.username)
     return render_template('index.html', form=form, name='Guest')
