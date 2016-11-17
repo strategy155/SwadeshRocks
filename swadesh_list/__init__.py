0aebc07d8de605388e5a474daa735b96247bf178
Marc <strategy155@gmail.com>
2016-11-17 15:22:31 +0300

    added pagination

--- swadesh_list/__init__.py
+++ swadesh_list/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+
 import swadesh_list.index
 import swadesh_list.register
 import swadesh_list.about
@@ -11,3 +12,4 @@ import swadesh_list.json_download_page
 import swadesh_list.login
 import swadesh_list.search
 import swadesh_list.stats
+app.jinja_env.globals['url_for_other_page'] = swadesh_list.form.url_for_other_page
--- swadesh_list/form.py
+++ swadesh_list/form.py
@@ -1,29 +1,37 @@
 from swadesh_list import app
-from flask import abort, render_template
+from flask import abort, render_template, request, url_for
 from flask_sqlalchemy import Pagination
 import re
 PER_PAGE = 10
 FORM_ELEMS = 100
 
 
-@app.route('/form', defaults={'page' : 1})
-@app.route('/form/page/<int:page>')
-def form(page):
-    words = get_words_for_page(page, PER_PAGE, FORM_ELEMS)
-    if page !=1:
+@app.route('/form', defaults={'page_num' : 1})
+@app.route('/form/page/<int:page_num>')
+def form(page_num):
+    words = get_words_for_page(page_num, PER_PAGE, FORM_ELEMS)
+    if not words and page_num !=1:
         abort(404)
-    pagination = Pagination(page, PER_PAGE,FORM_ELEMS)
+    pagination = Pagination(page_num, PER_PAGE,FORM_ELEMS)
+    words = "haha"
     return render_template('form.html', pagination=pagination, words=words)
 
 
-def get_words_for_page(page, per_page, count):
+def get_words_for_page(page_num, per_page, count):
     list_of_words=get_swadesh_list()
-
+    first_word=page_num*(per_page-1)
+    page_words = list_of_words[first_word:count-page_num]
     return None
 
 
+def url_for_other_page(page):
+    url_args = request.view_args.copy()
+    url_args['page'] = page
+    return url_for(request.endpoint, **url_args)
+
+
 def get_swadesh_list():
-    with open('swadesh.txt', 'r') as t:
+    with open('\\static\\swadesh.txt', 'r') as t:
         lul = t.read().replace('#', '').replace('\t', '')
         swadesh_list = (re.sub(' ', '', lul).split('\n'))
     return swadesh_list
